173ed9ea150ad8f7fbfaba0c7fe6bde8b8768969
Daniel Davi Libâneo <daniel.libaneo@gmail.com>
2020-12-19 18:53:11 -0300

    initial commit

---
+++ Orus_UserAccessAndManagement/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Orus_UserAccessAndManagement.Startup))]
+namespace Orus_UserAccessAndManagement
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
