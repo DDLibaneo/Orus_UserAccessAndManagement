27daeeb651780b680c78414e6b35b5cc816543db
Daniel Davi Libâneo <daniel.libaneo@gmail.com>
2020-12-20 13:33:24 -0300

    enable-migrations

---
+++ Orus_UserAccessAndManagement/Migrations/Configuration.cs
@@ -0,0 +1,23 @@
+﻿namespace Orus_UserAccessAndManagement.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<Orus_UserAccessAndManagement.Models.ApplicationDbContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+        }
+
+        protected override void Seed(Orus_UserAccessAndManagement.Models.ApplicationDbContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method
+            //  to avoid creating duplicate seed data.
+        }
+    }
+}
