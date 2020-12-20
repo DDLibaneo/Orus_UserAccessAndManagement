f2ad9d740e5f032956620c2227875df9456ed60e
Daniel Davi Libâneo <daniel.libaneo@gmail.com>
2020-12-20 12:35:57 -0300

    add OnModelCreating to DbContext

--- Orus_UserAccessAndManagement/Models/ApplicationDbContext.cs
+++ Orus_UserAccessAndManagement/Models/ApplicationDbContext.cs
@@ -1,12 +1,17 @@
 ﻿using Microsoft.AspNet.Identity.EntityFramework;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
 
 namespace Orus_UserAccessAndManagement.Models
 {
     public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
-        public ApplicationDbContext()
-            : base("DefaultConnection", throwIfV1Schema: false)
+        public ApplicationDbContext() : base("DefaultConnection", throwIfV1Schema: false) { }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
         {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+            base.OnModelCreating(modelBuilder);
         }
 
         public static ApplicationDbContext Create()
